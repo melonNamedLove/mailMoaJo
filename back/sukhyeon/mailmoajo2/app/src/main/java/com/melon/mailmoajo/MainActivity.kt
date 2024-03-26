12fb6153b6523299ac6d5cb8d7ea441a558124da
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2024-03-27 07:26:34 +0900

    네이티브 구글 로그인 구현 완료

--- back/sukhyeon/mailmoajo2/app/src/main/java/com/melon/mailmoajo/MainActivity.kt
+++ back/sukhyeon/mailmoajo2/app/src/main/java/com/melon/mailmoajo/MainActivity.kt
@@ -1,70 +1,106 @@
 package com.melon.mailmoajo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.melon.mailmoajo.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var resultLauncher : ActivityResultLauncher<Intent>
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        account?.let {
+            Toast.makeText(this, "Logged In", Toast.LENGTH_SHORT).show()
+        }?: Toast.makeText(this, "Not Yet", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+
+        setResultSignUp()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+        with(binding){
+            btnSignIn.setOnClickListener{
+                signIn()
+            }
+
+            btnSignOut.setOnClickListener {
+                signOut()
+            }
+        }
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
         setContentView(binding.root)
+
     }
-//    private lateinit var auth: FirebaseAuth
-// ...
-// Initialize Firebase Auth
-//    auth = Firebase.auth
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//
-//    paoverride fun onStart() {
-//        super.onStart()
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        var currentUser = auth.getCurrentUser()
-//        updateUI(currentUser);
-//    }
-//
-//
-//    signInRequest = BeginSignInRequest.builder()
-//    .setGoogleIdTokenRequestOptions(
-//    BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-//    .setSupported(true)
-//    // Your server's client ID, not your Android client ID.
-//    .setServerClientId(getString(R.string.your_web_client_id))
-//    // Only show accounts previously used to sign in.
-//    .setFilterByAuthorizedAccounts(true)
-//    .build())
-//    .build()
-//
-//
-//    val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
-//    val idToken = googleCredential.googleIdToken
-//    when {
-//        idToken != null -> {
-//            // Got an ID token from Google. Use it to authenticate
-//            // with Firebase.
-//            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-//            auth.signInWithCredential(firebaseCredential)
-//                .addOnCompleteListener(this) { task ->
-//                    if (task.isSuccessful) {
-//                        // Sign in success, update UI with the signed-in user's information
-//                        Log.d(TAG, "signInWithCredential:success")
-//                        val user = auth.currentUser
-//                        updateUI(user)
-//                    } else {
-//                        // If sign in fails, display a message to the user.
-//                        Log.w(TAG, "signInWithCredential:failure", task.exception)
-//                        updateUI(null)
-//                    }
-//                }
-//        }
-//        else -> {
-//            // Shouldn't happen.
-//            Log.d(TAG, "No ID token!")
-//        }
-//    }
+    private fun setResultSignUp(){
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        //정상적으로 결과가 받아질 때 조건문 실행
+            if(result.resultCode == Activity.RESULT_OK){
+                val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleSignInResult(task)
+            }
+
+        }
+    }
+
+    private fun handleSignInResult(completedTask:Task<GoogleSignInAccount>){
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            val email = account?.email.toString()
+            val familyName = account?.familyName.toString()
+            val givenName = account?.givenName.toString()
+            val displayName = account?.displayName.toString()
+            val photoUrl = account?.photoUrl.toString()
+        }catch (e:ApiException){
+            //the api Exception status code indicates the detailed failure reason
+            //please refer to GoogleSignInStatusCodes class reference for more information
+            Log.w("failed","signInResult:failed code"+ e.statusCode)
+        }
+    }
+
+    private fun signIn(){
+        val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
+        resultLauncher.launch(signInIntent)
+
+    }
+
+    private fun signOut(){
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this){
+
+            }
+    }
+    private fun revokeAccess(){
+        mGoogleSignInClient.revokeAccess()
+            .addOnCompleteListener(this){
+
+            }
+    }
+
+
 }
