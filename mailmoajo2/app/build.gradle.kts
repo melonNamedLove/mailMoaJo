09d17067add80ed5bd62343a94130c7620fc5596
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2024-05-31 05:19:18 +0900

    테마 수정 등등

--- mailmoajo2/app/build.gradle.kts
+++ mailmoajo2/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.melon.mailmoajo"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 35
 //        targetSdkPreview = "VanillaIceCream"
         versionCode = 1
@@ -83,6 +83,7 @@ dependencies {
     implementation("androidx.annotation:annotation:1.8.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
