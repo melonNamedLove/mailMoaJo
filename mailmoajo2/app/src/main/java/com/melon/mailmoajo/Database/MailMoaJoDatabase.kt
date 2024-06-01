5b5caa5189182d3c09e2288d6c3b5abcbb0bf5ea
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2024-06-01 20:39:09 +0900

    outlook, gmail table 생성 done

--- mailmoajo2/app/src/main/java/com/melon/mailmoajo/Database/MailMoaJoDatabase.kt
+++ mailmoajo2/app/src/main/java/com/melon/mailmoajo/Database/MailMoaJoDatabase.kt
@@ -3,24 +3,31 @@ package com.melon.mailmoajo.Database
 import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.melon.mailmoajo.DAOs.GmailDao
+import com.melon.mailmoajo.DAOs.OutlookDao
 import com.melon.mailmoajo.DAOs.contactDao
 import com.melon.mailmoajo.DAOs.mailDao
 import com.melon.mailmoajo.DAOs.mailfolderDao
+import entities.Gmails
+import entities.OutlookMails
 import entities.contacts
 import entities.mails
 import entities.orderedMailFolders
 
 //@Database(entities = arrayOf(User::class, Student::class), version = 1)
-@Database(entities = arrayOf(contacts::class, orderedMailFolders::class, mails::class), version = 3, exportSchema = true,
+@Database(entities = arrayOf(contacts::class, orderedMailFolders::class, mails::class, Gmails::class, OutlookMails::class), version = 4, exportSchema = true,
     autoMigrations = [
         AutoMigration (from = 1, to = 2),
-        AutoMigration (from = 2, to = 3)
+        AutoMigration (from = 2, to = 3),
+        AutoMigration (from = 3, to = 4)
     ]
 )
 abstract  class MailMoaJoDatabase :RoomDatabase() {
     abstract  fun contactDao():contactDao
     abstract  fun mailfolderDao():mailfolderDao
     abstract  fun mailDao(): mailDao
+    abstract  fun gmailDao(): GmailDao
+    abstract  fun outlookDao(): OutlookDao
 //    abstract  fun mailfolderDao():mailfolderDao
 
 //    companion object {
